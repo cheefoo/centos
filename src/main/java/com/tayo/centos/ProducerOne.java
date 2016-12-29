fdaa554176a845dd63095567e68436043cae446b
Olajide <temitayo@amazon.com>
2016-12-29 10:13:52 -0600

    Refactored and added comments

--- src/main/java/com/tayo/centos/ProducerOne.java
+++ src/main/java/com/tayo/centos/ProducerOne.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.kinesis.producer.*;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.tayo.centos.util.CentosUtils;
 import org.apache.commons.lang.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -12,19 +13,14 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
-import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 /**
  * Created by temitayo on 11/3/16.
@@ -32,27 +28,13 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 public class ProducerOne
 {
     private static final Logger log = LoggerFactory.getLogger(ProducerOne.class);
-    private static final String DELIM = ",";
-    public static final String STREAM_NAME = "centosstream";
-    //public static final String KPL_TMP_DIR = "/Users/temitayo/workspace/centos";
-    private static final Random RANDOM = new Random();
-    private List<UserDAO> userList;
-    //private final WatchService watcher;
-    private static long id = 0;
+    private static long id = 1;  //Id used to create unique objects to be sent to Kinesis
 
-    public void setUserList(List<UserDAO> userList)
-    {
-        this.userList = userList;
-    }
 
-    public List<UserDAO> getUserList()
-    {
-        return userList;
-    }
 
-    public static KinesisProducer getKinesisProducer() throws IOException
+    private static KinesisProducer getKinesisProducer() throws IOException
     {
-    	String kpltempdir = getKPLTempDir();
+    	String kpltempdir = CentosUtils.getProperties().getProperty("kpltempdir");
         KinesisProducerConfiguration config = KinesisProducerConfiguration.fromPropertiesFile("default_config.properties");
         config.setCredentialsProvider(new DefaultAWSCredentialsProviderChain());
         config.setTempDirectory(kpltempdir);
@@ -62,19 +44,15 @@ public class ProducerOne
 
     public static void main (String [] args) throws IOException
     {
-        //Read in user list from log file and set in
-        //List<UserDAO> userList = getDataObjects();
-        ProducerOne one = new ProducerOne();
-        //one.setUserList(userList);
-        String streamName = getStreamNameProps();
+
+        String streamName = CentosUtils.getProperties().getProperty("streamname");
         KinesisProducer producer = getKinesisProducer();
 
-
-        final List failedPutList = new ArrayList();
         final FutureCallback<UserRecordResult> callback = new FutureCallback<UserRecordResult>()
         {
             @Override
-            public void onFailure(Throwable t) {
+            public void onFailure(Throwable t)
+            {
 
                 if (t instanceof UserRecordFailedException)
                 {
@@ -107,11 +85,7 @@ public class ProducerOne
                 long totalTime = userRecordResult.getAttempts().stream()
                         .mapToLong(a -> a.getDelay() + a.getDuration())
                         .sum();
-                // Only log with a small probability, otherwise it'll be very
-                // spammy
-               /* if (RANDOM.nextDouble() < 1e-5)
-                {*/
-                    log.info(String.format(
+                                 log.info(String.format(
                             "Succesfully put record,  "
                                     + " sequenceNumber=%s, "
                                     + "shardId=%s, took %d attempts, "
@@ -119,11 +93,13 @@ public class ProducerOne
                             userRecordResult.getSequenceNumber(),
                             userRecordResult.getShardId(), userRecordResult.getAttempts().size(),
                             totalTime));
-               /* }*/
+
             }
         };
-        String fileLocationToProcess = getFileLocationProps();
+
+        String fileLocationToProcess = CentosUtils.getProperties().getProperty("filelocation");//Retrieves file location to process
         log.info("File Location is " + fileLocationToProcess);
+
         while(true)
         {
             List<String>filesToProcess =   getFilesToSend(new File(fileLocationToProcess));
@@ -135,8 +111,18 @@ public class ProducerOne
                     List<UserDAO> userList = null;
                     try
                     {
-                        userList = getDataObjects(file);
+                        userList = getDataObjects(file); //gets the user objects from file into a list
                         log.info("Obtained user event list : " + userList.size());
+                        log.info("About to delete completed file : " + file);
+                        if(deleteReadFile(file))
+                        {
+                           log.info("file +  " + file + " "+ "deleted successfully");
+                        }
+                        else
+                        {
+                            log.error("File delete for " + file + " " + "failed");
+                        }
+
                     } catch (Exception e)
                     {
                         e.printStackTrace();
@@ -175,6 +161,27 @@ public class ProducerOne
         return new BigInteger(128, new Random()).toString(10);
     }
 
+    /*
+    * Deletes already read files, alternatively the files can be moved to a new location
+    * Care should be taken not to generate the files in the same directory which the KPL is watching, it may cause ugly scenarios
+     */
+    private static boolean deleteReadFile(String  fileName)
+    {
+        //deleting file
+        File file = new File(fileName);
+        if(file.delete())
+        {
+            log.info(file.getName() + " has been deleted after reading into list");
+            return true;
+        }
+        else
+        {
+            log.error("Unable to delete file "+ file.getName());
+
+        }
+        return false;
+    }
+
     private static List<UserDAO> getDataObjects(String fileName) throws Exception
     {
         List<UserDAO> userObjectList= new ArrayList<UserDAO>();
@@ -211,84 +218,15 @@ public class ProducerOne
         }
 
         log.info("Finished reading all objects from file");
-        //deleting file
-        File file = new File(fileName);
-       /* if(file.delete())
-        {
-            log.info(file.getName() + " has been deleted after reading into list");
-        }
-        else
-        {
-            log.error("Unable to delete file "+ file.getName());
-        }*/
+
         return userObjectList;
     }
 
 
-
-    private static String getFileToProcess() throws IOException
-    {
-        Path directory = Paths.get(getFileLocationProps());
-        Path child = null;
-        WatchService watcher = FileSystems.getDefault().newWatchService();
-        directory.register(watcher, ENTRY_CREATE);
-        for(;;)
-        {
-            // wait for key to be signaled
-            WatchKey key = null;
-            try
-            {
-                key = watcher.take();
-            }
-            catch(InterruptedException x)
-            {
-                log.error(x.toString());
-                System.exit(1);
-            }
-            for(WatchEvent<?> event: key.pollEvents())
-            {
-                WatchEvent.Kind kind = event.kind();
-
-                if (kind == OVERFLOW)
-                {
-                    continue;
-                }
-                WatchEvent<Path> ev = (WatchEvent<Path>)event;
-                Path filename = ev.context();
-                //Verify that the new file is a text file.
-                try
-                {
-                    child = directory.resolve(filename);
-                    if (!Files.probeContentType(child).equals("text/plain"))
-                    {
-                        System.err.format("New file '%s' is not a plain text file.%n", filename);
-                        continue;
-                    }
-                }
-                catch (IOException x)
-                {
-                    log.error(x.toString());
-                    continue;
-                }
-
-            }
-
-            //Reset the key -- this step is critical if you want to receive
-            //further watch events. If the key is no longer valid, the directory
-            //is inaccessible so exit the loop.
-            boolean valid = key.reset();
-            if (!valid)
-            {
-                break;
-            }
-
-        }
-
-        return child.toString();
-
-    }
-
-    public static List<String> getFilesToSend(final File folder)
+    /*
+    Retrieves all files to be processed
+     */
+    private static List<String> getFilesToSend(final File folder)
     {
         List<String> filesToSend = new ArrayList<String>();
         for (final File fileEntry : folder.listFiles())
@@ -304,39 +242,6 @@ public class ProducerOne
         }
 
         return filesToSend;
-    }
-
-    public static String getFileLocationProps() throws IOException
-    {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream input =  classLoader.getResourceAsStream("db.properties");
-        java.util.Properties prop = new Properties();
-        prop.load(input);
-
-        return prop.getProperty("filelocation");
-
-    }
-
-    public static String getStreamNameProps() throws IOException
-    {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream input = classLoader.getResourceAsStream("db.properties");
-        java.util.Properties prop = new Properties();
-        prop.load(input);
-
-        return prop.getProperty("streamname");
-
-    }
-    
-    public static String getKPLTempDir() throws IOException
-    {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream input =  classLoader.getResourceAsStream("db.properties");
-        java.util.Properties prop = new Properties();
-        prop.load(input);
-
-        return prop.getProperty("kpltempdir");
-
     }
 
 }
